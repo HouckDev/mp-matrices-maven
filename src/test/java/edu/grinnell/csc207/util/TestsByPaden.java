ac89ad7cd51883178f69abd20a04618a02ef23c0
HouckDev <houckdeveloper@gmail.com>
2024-10-17 16:34:21 -0500

    Create TestsByPaden

---
+++ src/test/java/edu/grinnell/csc207/util/TestsByPaden.java
@@ -0,0 +1,30 @@
+package edu.grinnell.csc207.util;
+
+import static edu.grinnell.csc207.util.MatrixAssertions.assertMatrixEquals;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+/**e
+ * A variety of tests for the Matrix class.
+ *
+ * @author Paden Houck
+ */
+class TestMatrix {
+    /**
+     * Make sure that we can construct matrices.
+     */
+    @Test
+    public void testConstruct() {
+  
+      Matrix<Integer> matrix1x1 = new MatrixV0<Integer>(1, 1);
+      assertMatrixEquals(new Integer[][] {{null}}, matrix1x1,
+          "default 1x1 matrix");
+    }
+
+} // TestMatrix
